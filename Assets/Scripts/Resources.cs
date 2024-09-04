1ee3713faed4a88b670178283d8a97174e0f4b92
Filomolusca <pedromello.virt@yahoo.com.br>
2024-09-04 18:59:01 -0300

    teste dragndrop

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resource
+public class Resource : MonoBehaviour
 {
     public string Name { get; private set; }
     public int Quantity { get; set; }
